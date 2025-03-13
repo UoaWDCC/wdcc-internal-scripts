8448d514d0064cb2d4cdca77073fa1d2c9b8fce3
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-14 10:46:25 +1300

    :sparkles: add final objective score logging to allocation script

--- allocation/index.ts
+++ allocation/index.ts
@@ -1,27 +1,32 @@
 // --- This file mainly for I/O ---
 
-import { parseCsvProjects } from "../common/csvParser/parseCsvProjects.js";
 import { parseProcessedCsvApplicants } from "../common/csvParser/parseCsvProcessedApplicants.js";
+import { parseCsvProjects } from "../common/csvParser/parseCsvProjects.js";
+import { config } from "../config.js";
 import { stableMatching } from "./algorithms/stableMatching.js";
+import { calculateTotalUtility } from "./helper/objective.js";
 
-const Allocation = async() => {
-    const projectsDataLocation = "./data/projectsData.csv"
-    const applicantsDataLocation = "./data/processedApplicants.csv" // get processed applicants
-    console.log("Running allocation script");
+const allocate = async() => {
+  const projectsDataLocation = "./data/projectsData.csv"
+  const applicantsDataLocation = "./data/processedApplicants.csv" // get processed applicants
+  console.log("Running allocation script");
+  const {A, B, C, D} = config.allocation;
 
-    console.log("Parsing Applicants CSV...");
-    const applicants = await parseProcessedCsvApplicants(applicantsDataLocation)
-    
-    console.log("Parsing Projects CSV...");
-    const projectsData =  await parseCsvProjects(projectsDataLocation)
-  
+  console.log("Parsing Applicants CSV...");
+  const applicants = await parseProcessedCsvApplicants(applicantsDataLocation)
 
-    console.log("Parsed! Running allocation algorithm...");
-    const res = stableMatching(applicants, projectsData)
+  console.log("Parsing Projects CSV...");
+  const projectsData =  await parseCsvProjects(projectsDataLocation)
 
-    console.log(`Allocated ${res.length} projects! Writing to CSV...`);
 
-    console.log("Allocation script complete 🚀");
+  console.log("Parsed! Running allocation algorithm...");
+  const allocations = stableMatching(applicants, projectsData)
+  const finalObjectiveScore = calculateTotalUtility(allocations, A, B, C, D)
+
+  console.log(`Allocated ${allocations.length} projects! Objective score: ${finalObjectiveScore}`);
+  console.log(`Writing to CSVs...`);
+
+  console.log("Allocation script complete 🚀");
 }
 
-Allocation()
+allocate();
