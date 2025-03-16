37661a43f1d81c8fcecb65b7d1ce35705d65843d
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-16 13:32:03 +1300

    :tshirt: format models

--- common/models.ts
+++ common/models.ts
@@ -1,30 +1,30 @@
-    export type Applicant = {
-      timestamp: Date;
-      id: string;
-      name: string;
-      email: string;
-      github: string;
-      major: string;
-      rolePreference: string;
-      skills: string[];
-      backendPreference: number;
-      frontendExperience: number;
-      backendExperience: number;
-      designExperience: number;
-      testingExperience: number; //not too important ?
-      projectChoices: string[]; //index 0 is highest preferences
-      passionBlurb: string; // not used
-      portfolioLink: string; //not used
-      additionalInfo: string; //not used
-      execComments: string; // not used
-      creativityHire: string; // if 
-      rizzLevel: number; // not used
-    };
+export type Applicant = {
+  timestamp: Date;
+  id: string;
+  name: string;
+  email: string;
+  github: string;
+  major: string;
+  rolePreference: string;
+  skills: string[];
+  backendPreference: number;
+  frontendExperience: number;
+  backendExperience: number;
+  designExperience: number;
+  testingExperience: number; //not too important ?
+  projectChoices: string[]; //index 0 is highest preferences
+  passionBlurb: string; // not used
+  portfolioLink: string; //not used
+  additionalInfo: string; //not used
+  execComments: string; // not used
+  creativityHire: string; // if
+  rizzLevel: number; // not used
+};
 
 export type Project = {
   id: string;
   name: string;
-  backendDifficulty: number; 
+  backendDifficulty: number;
   frontendDifficulty: number;
   backendWeighting: number
   priority: number;
