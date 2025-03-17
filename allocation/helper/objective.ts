67bc9a2e9f148b92791988dc27022eb2c40c484e
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-18 11:39:02 +1300

    add extra logging

--- allocation/helper/objective.ts
+++ allocation/helper/objective.ts
@@ -14,8 +14,9 @@ export function calculateTotalUtility(allocations: Allocation[]): number {
  *
  * @param allocation An allocation of applicants to a single project
  */
-export function calculateUtilityOfAllocation(allocation: Allocation): number {
+export function calculateUtilityOfAllocation(allocation: Allocation, log: boolean = false): number {
     const { project, applicants } = allocation;
+    const n = applicants.length;
 
     // I need these names to be short for my sanity sorry
     let projectPrefScore = 0; // Overall PROJECT CHOICE satisfaction metric
@@ -30,18 +31,28 @@ export function calculateUtilityOfAllocation(allocation: Allocation): number {
     }
 
     // Overall ROLE (FE/BE) dissatisfaction metric
-    const targetBePrefSum = applicants.length * project.backendWeighting;
-    const rolePrefDeviation = Math.abs(bePrefSum - targetBePrefSum);
+    const targetBePrefSum = n * project.backendWeighting * 5/7; // project.backendWeighting out of 7 for some reason
+    const rolePrefDeviation = Math.abs(bePrefSum - targetBePrefSum) * 5/4; // Max deviation is 4 pp
 
     // Experience level metrics
-    const beExpScore = beExpSum * project.backendDifficulty;
-    const feExpScore = feExpSum * project.frontendDifficulty;
+    const beExpScore = beExpSum * project.backendDifficulty / 5;
+    const feExpScore = feExpSum * project.frontendDifficulty / 5;
 
     // TODO needs priority and role preference
     const { A, B, C, D } = config.allocation;
+    const objectiveScore = A * projectPrefScore;
+    // const objectiveScore = A * projectPrefScore - B * rolePrefDeviation + C * beExpScore + D * feExpScore;
 
-    return A * projectPrefScore;
-    return A * projectPrefScore - B * rolePrefDeviation + C * beExpScore + D * feExpScore;
+    // Logging (bit of a hack...)
+    if (log) {
+        console.log(`  Proj pref: ${projectPrefScore.toFixed(2)}/${n*5}`);
+        console.log(`  Role pref: ${(n*5 - rolePrefDeviation).toFixed(2)}/${n*5}`);
+        console.log(`  BE exp:    ${beExpScore.toFixed(2)}/${n*5}`);
+        console.log(`  FE exp:    ${feExpScore.toFixed(2)}/${n*5}`);
+        console.log(`  Objective: ${objectiveScore.toFixed(2)}`);
+    }
+
+    return objectiveScore;
 }
 
 /** 5 for first choice, 4 for second choice ... 0 for not chosen */
--- allocation/helper/utils.ts
+++ allocation/helper/utils.ts
@@ -1,4 +1,5 @@
-import { Allocation, Applicant } from "../../common/models.js";
+import { Allocation } from "../../common/models.js";
+import { calculateUtilityOfAllocation } from "./objective.js";
 
 export function logAllocationRanking(allocation: Allocation[]) {
     for (const projectAllocation of allocation) {
@@ -10,28 +11,29 @@ export function logAllocationRanking(allocation: Allocation[]) {
     }
 }
 
-export function logAllocationRankingList(allocation: Allocation[]) {
-    for (const projectAllocation of allocation) {
-        console.log(projectAllocation.project.name);
-        console.log(projectAllocation.applicants.length);
-        const temp = {
-            0: [] as Applicant[],
-            1: [] as Applicant[],
-            2: [] as Applicant[],
-            3: [] as Applicant[],
-            4: [] as Applicant[],
-            5: [] as Applicant[],
+export function logAllocationRankingList(allocations: Allocation[]) {
+    for (const projectAllocation of allocations) {
+        console.log();
+        console.log(`PROJECT ${projectAllocation.project.name} (count: ${projectAllocation.applicants.length})`);
+        calculateUtilityOfAllocation(projectAllocation, true);
+        const temp: Record<number, string[]> = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
         }
         for (const applicant of projectAllocation.applicants) {
             temp[applicant.projectChoices.indexOf(projectAllocation.project.name) + 1].push(applicant.name)
         }
 
-        console.log(`  0 (${temp[0].length}): ${temp[0].join(", ")}`);
-        console.log(`  1 (${temp[1].length}): ${temp[1].join(", ")}`);
-        console.log(`  2 (${temp[2].length}): ${temp[2].join(", ")}`);
-        console.log(`  3 (${temp[3].length}): ${temp[3].join(", ")}`);
-        console.log(`  4 (${temp[4].length}): ${temp[4].join(", ")}`);
-        console.log(`  5 (${temp[5].length}): ${temp[5].join(", ")}`);
+        console.log(`  1  (${temp[1].length}): ${temp[1].join(", ")}`);
+        console.log(`  2  (${temp[2].length}): ${temp[2].join(", ")}`);
+        console.log(`  3  (${temp[3].length}): ${temp[3].join(", ")}`);
+        console.log(`  4  (${temp[4].length}): ${temp[4].join(", ")}`);
+        console.log(`  5  (${temp[5].length}): ${temp[5].join(", ")}`);
+        console.log(`  6+ (${temp[0].length}): ${temp[0].join(", ")}`);
 
     }
 }
