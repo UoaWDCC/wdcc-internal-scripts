19a2dd01f2b2731e877ec7d2e6b6b351a0b4bb43
Kot <89110272+Kot6603@users.noreply.github.com>
2025-03-18 14:53:46 +1300

    feat: clearly the better algorithm

--- allocation/index.ts
+++ allocation/index.ts
@@ -22,8 +22,8 @@ const allocate = async () => {
 
     // Algorithm
     console.log("Parsed! Running allocation algorithm...");
-    // const allocations = stableMatching(applicants, projectsData);
-    const allocations = randomHeuristicAscent(applicants, projectsData);
+    const allocations = stableMatching(applicants, projectsData);
+    // const allocations = randomHeuristicAscent(applicants, projectsData);
     // const allocations = powerOfFriendship(applicants, projectsData);
 
     const randomAllocations = randomlyAllocate(projectsData, applicants);
