6cad7b32004efaf1086139f981c2932ec98bafc8
Kot <89110272+Kot6603@users.noreply.github.com>
2026-03-15 23:28:05 +1100

    feat: general applicant csv parser

--- projects-allocation-script/cmd/allocate.ts
+++ projects-allocation-script/cmd/allocate.ts
@@ -2,37 +2,33 @@
 import { powerOfFriendship } from "../allocation/algorithms/powerOfFriendship.js";
 import { randomlyAllocate } from "../allocation/helper/random.js";
 import { logAllocationRankingList } from "../allocation/helper/utils.js";
-import { parseProcessedCsvApplicants } from "../csv/parseCsvProcessedApplicants.js";
 import { parseCsvProjects } from "../csv/parseCsvProjects.js";
 import { writeCsv } from "../csv/writeCsv.js";
-import { config } from "../config.js";
+import { allocationConfig } from "../config/scriptConfig.js"
+import { parseApplicantsCsv } from "../csv/parseApplicants.js";
+import { Applicant, Project } from "../common/types.js";
 
-const allocate = async () => {
-    console.log("Running allocation script");
-    const { inFileApplicants, inFileTeams, outFileFormat } = config.allocation;
+console.log("[INFO] Running allocation script")
+const { inputFileApplicants, inputFileProjects, outputFileFormat } = allocationConfig
 
-    // Input
-    console.log("Parsing Applicants CSV...");
-    const applicants = await parseProcessedCsvApplicants(inFileApplicants);
-    console.log("Parsing Projects CSV...");
-    const projectsData = await parseCsvProjects(inFileTeams);
+console.log("[INFO] Parsing Applicants CSV...");
+const applicants: Applicant[] = await parseApplicantsCsv(inputFileApplicants, false);
+console.log("Parsing Projects CSV...");
+const projectsData: Project[] = await parseCsvProjects(inputFileProjects);
 
-    // Algorithm
-    console.log("Parsed! Running allocation algorithm...");
-    const allocations = powerOfFriendship(applicants, projectsData);
-    const randomAllocations = randomlyAllocate(projectsData, applicants);
-    logAllocationRankingList(allocations, randomAllocations);
+// Algorithm
+console.log("Parsed! Running allocation algorithm...");
+const allocations = powerOfFriendship(applicants, projectsData);
+const randomAllocations = randomlyAllocate(projectsData, applicants);
+logAllocationRankingList(allocations, randomAllocations);
 
-    // Output
-    console.log(`Writing to CSVs...`);
-    allocations.forEach((allocation) => {
-        const safeProjectName = allocation.project.name.replace(/[\\/:.]/, "_");
-        console.log(`${safeProjectName} (${allocation.project.id}) has ${allocation.applicants.length} applicants.`);
-        const outFileName = outFileFormat.replace("<team>", safeProjectName);
-        writeCsv(allocation.applicants, outFileName);
-    });
+// Output
+console.log(`Writing to CSVs...`);
+allocations.forEach((allocation) => {
+	const safeProjectName = allocation.project.name.replace(/[\\/:.]/, "_");
+	console.log(`${safeProjectName} (${allocation.project.id}) has ${allocation.applicants.length} applicants.`);
+	const outFileName = outputFileFormat.replace("<team>", safeProjectName);
+	writeCsv(allocation.applicants, outFileName);
+});
 
-    console.log("Allocation script complete 🚀");
-};
-
-allocate();
+console.log("Allocation script complete 🚀");
